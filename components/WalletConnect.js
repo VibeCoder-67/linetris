06d4779d62d9c1aafe44216228304c586668657f
Vibe Coder <cedricchungtf@gmail.com>
2025-11-29 15:54:38 +0800

    created connect wallet component

---
+++ components/WalletConnect.js
@@ -0,0 +1,11 @@
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+
+const WalletConnect = () => {
+    return (
+        <div className="fixed top-6 right-6 z-[60]">
+            <DynamicWidget />
+        </div>
+    );
+};
+
+export default WalletConnect;
