22d4c02f41007276ce1a13257fc3bbd660c506c7
Vibe Coder <cedricchungtf@gmail.com>
2025-11-29 15:54:49 +0800

    wrap providers in layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import "@/styles/globals.css";
 
+const environmentId = process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID || "2762a57b-faa4-41ce-9f16-abff9300e2c9";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <Component {...pageProps} />
+    </DynamicContextProvider>
+  );
 }
